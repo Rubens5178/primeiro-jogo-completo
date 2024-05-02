2ffbb9b88a87ffbe1219a7d6292f3930724b2e9d
Rubens5178 <166870080+Rubens5178@users.noreply.github.com>
2024-05-01 22:23:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function exibirTextoNaTela(tag,texto){
     campo.innerHTML = texto;
 }
 
-function exibitMensagemInicial{
+function exibitMensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
